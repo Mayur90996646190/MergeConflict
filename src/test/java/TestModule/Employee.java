c296653ebe167e5cdcff9e10bf6b0157b0147474
misampelliwar@gmail.com <misampelliwar@gmail.com>
2023-12-24 09:26:00 +0530

    datatype sal changed

--- src/test/java/TestModule/Employee.java
+++ src/test/java/TestModule/Employee.java
@@ -3,7 +3,7 @@ package TestModule;
 public class Employee {
 	private String name;
 	private int  age ;
-	private int salary;
+	private double salary;
 	public Employee(String name, int age, int salary) {
 		super();
 		this.name = name;
